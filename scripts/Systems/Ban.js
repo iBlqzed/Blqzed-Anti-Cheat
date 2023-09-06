4741348e0665482b44ca9b30c33c5529871c4401
iBlqzed <iBlqzed@users.noreply.github.com>
2023-09-06 01:35:58 -0500

    fixed ban

--- scripts/Systems/Ban.js
+++ scripts/Systems/Ban.js
@@ -4,7 +4,7 @@ import { config, timeFormat } from "../globalVars.js";
 world.afterEvents.playerSpawn.subscribe(data => {
 	if (!data.initialSpawn) return
 	if (data.player.isAdmin()) return config.banDB.delete(data.player.name)
-	const arr = config.banDB.get(data.player)
+	const arr = config.banDB.get(data.player.name)
 	if (!arr) return
 	const now = Date.now()
 	if (arr[0] === "permanent" || arr[0] < now) return player.kick(arr[1] + ". Unbanned: " + timeFormat(arr[0] - now), true)
